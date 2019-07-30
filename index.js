f01b29be9dfdbed75ba27a34b4a3b36d531ad1b9
Andy Cary <andy@andycary.com>
2019-07-30 21:02:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function viewCart() {
 
 function total() {
   // The `total()` function accepts no arguments, iterates through the `cart` array, and returns the current total value of the items in the cart.
+  
+  // var cart = [];
 
   return myTotal;
 }
