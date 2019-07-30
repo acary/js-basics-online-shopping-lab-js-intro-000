dcb6237fee24b409e5706ae63b27b3cad6dc40bc
Andy Cary <andy@andycary.com>
2019-07-30 20:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  // If the item is present in the cart, the function should remove the object from the cart and then return the updated cart.
+  // HINT: Check each object's itemName value key to see if it matches the parameter, then remove it if it matches. You might find Array.prototype.splice() to be useful.
+  // If the cart does not contain a matching item, the function should return That item is not in your cart.
 }
 
 function placeOrder(cardNumber) {
